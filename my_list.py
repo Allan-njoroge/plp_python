acf93d7df9dbe0f27a4e22b8fd3cab3bb12794ef
Allan-njoroge <allandavis254@gmail.com>
2024-03-01 15:02:31 +0300

    List

---
+++ my_list.py
@@ -0,0 +1,25 @@
+#creating the empty list
+my_list = []
+
+#appending elements to list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#inserting the value 15 in the second position
+my_list[1] = 15
+
+#extending the list
+my_list.extend([50, 60, 70])
+
+#removing the last element
+my_list.pop()
+
+#sorting the list in ascending order
+list_sort = sorted(my_list)
+
+#finding and printing the index of 30
+index_30 = my_list.index(30)
+
+print(index_30)
